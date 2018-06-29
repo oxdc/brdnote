413ad363708d2c13c9833a6b6bb59a62d945f919
oxdc <frnet2016@gmail.com>
2018-06-29 21:00:55 +0800

    [init]

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'brdnote'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
