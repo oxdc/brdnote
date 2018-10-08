f7c25d500b86a146a5db16f9bbf6075962252df4
oxdc <frnet2016@gmail.com>
2018-10-08 19:34:24 +0800

    [update] open action

--- src/renderer/uitls/commands/open.js
+++ src/renderer/uitls/commands/open.js
@@ -12,89 +12,95 @@ export function open (vueRoot, callback, path = null) {
     return
   }
 
-  remote.dialog.showOpenDialog({
-    filters: [
-      { name: 'Notes', extensions: ['brdnote'] },
-      { name: 'Notebooks', extensions: ['brdnb'] },
-      { name: 'Plain texts', extensions: ['txt', 'md'] },
-      { name: 'All Files', extensions: ['*'] }
-    ]
-  }, (fileNames) => {
-    if (fileNames === undefined || fileNames[0] === undefined) {
-      return
-    }
-
-    if (!path) path = fileNames[0]
-
-    fs.readFile(path, 'utf-8', (err, content) => {
-      if (err) {
-        vueRoot.$Notice.error({
-          title: 'Error',
-          desc: 'An error ocurred while reading the file :' + err.message
-        })
-        closeDocument(vueRoot)
+  if (path) {
+    openDocument(path, vueRoot)
+  } else {
+    remote.dialog.showOpenDialog({
+      filters: [
+        { name: 'Notes', extensions: ['brdnote'] },
+        { name: 'Notebooks', extensions: ['brdnb'] },
+        { name: 'Plain texts', extensions: ['txt', 'md'] },
+        { name: 'All Files', extensions: ['*'] }
+      ]
+    }, (fileNames) => {
+      if (fileNames === undefined || fileNames[0] === undefined) {
         return
       }
 
-      if (content.startsWith('ENCRYPTED')) {
-        vueRoot.$store.commit('updateEncryptionStatus', true)
-      } else {
-        vueRoot.$store.commit('updateEncryptionStatus', false)
-      }
-
-      if (content.startsWith('ENCRYPTED')) {
-        content = content.substring(9)
-        var password = ''
-        vueRoot.$Modal.confirm({
-          render: (h) => {
-            return h('Input', {
-              props: {
-                value: password,
-                autofocus: true,
-                type: 'password',
-                placeholder: 'Please enter your password ...'
-              },
-              on: {
-                input: (value) => {
-                  password = value
-                }
-              }
-            })
-          },
-          onOk: () => {
-            try {
-              content = decryptContent(JSON.parse(atou(content)), password)
-              extractContent(content, vueRoot)
-              vueRoot.$store.commit('updatePassword', {
-                password: password
-              })
-              vueRoot.$Notice.success({
-                title: 'Success',
-                desc: ''
-              })
-              vueRoot.$store.commit('updatePath', {
-                path: path
-              })
-            } catch (err) {
-              vueRoot.$Notice.error({
-                title: 'Error',
-                desc: 'Password error. ' + err.message
-              })
-              closeDocument(vueRoot)
-            }
-          }
-        })
-      } else {
-        extractContent(content, vueRoot)
-        vueRoot.$store.commit('updatePath', {
-          path: path
-        })
-      }
-
-      if (typeof callback === 'function') {
-        callback()
-      }
+      openDocument(fileNames[0], vueRoot)
     })
+  }
+
+  if (typeof callback === 'function') {
+    callback()
+  }
+}
+
+function openDocument (path, vueRoot) {
+  fs.readFile(path, 'utf-8', (err, content) => {
+    if (err) {
+      vueRoot.$Notice.error({
+        title: 'Error',
+        desc: 'An error ocurred while reading the file :' + err.message
+      })
+      closeDocument(vueRoot)
+      return
+    }
+
+    if (content.startsWith('ENCRYPTED')) {
+      vueRoot.$store.commit('updateEncryptionStatus', true)
+    } else {
+      vueRoot.$store.commit('updateEncryptionStatus', false)
+    }
+
+    if (content.startsWith('ENCRYPTED')) {
+      content = content.substring(9)
+      var password = ''
+      vueRoot.$Modal.confirm({
+        render: (h) => {
+          return h('Input', {
+            props: {
+              value: password,
+              autofocus: true,
+              type: 'password',
+              placeholder: 'Please enter your password ...'
+            },
+            on: {
+              input: (value) => {
+                password = value
+              }
+            }
+          })
+        },
+        onOk: () => {
+          try {
+            content = decryptContent(JSON.parse(atou(content)), password)
+            extractContent(content, vueRoot)
+            vueRoot.$store.commit('updatePassword', {
+              password: password
+            })
+            vueRoot.$Notice.success({
+              title: 'Success',
+              desc: ''
+            })
+            vueRoot.$store.commit('updatePath', {
+              path: path
+            })
+          } catch (err) {
+            vueRoot.$Notice.error({
+              title: 'Error',
+              desc: 'Password error. ' + err.message
+            })
+            closeDocument(vueRoot)
+          }
+        }
+      })
+    } else {
+      extractContent(content, vueRoot)
+      vueRoot.$store.commit('updatePath', {
+        path: path
+      })
+    }
   })
 }
 
