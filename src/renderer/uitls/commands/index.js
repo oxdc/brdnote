aa224ff25ec3341cd2d5a6ff34940f4f09fc1b27
oxdc <frnet2016@gmail.com>
2019-03-14 09:04:28 +0800

    [Delete] v0.x

--- src/renderer/uitls/commands/index.js
+++
@@ -1,9 +0,0 @@
-const files = require.context('.', false, /\.js$/)
-const commands = {}
-
-files.keys().forEach(key => {
-  if (key === './index.js') return
-  commands[key.replace(/(\.\/|\.js)/g, '')] = files(key).default
-})
-
-export default commands
--- src/renderer/uitls/jobs/index.js
+++
@@ -1,9 +0,0 @@
-const files = require.context('.', false, /\.js$/)
-const jobs = {}
-
-files.keys().forEach(key => {
-  if (key === './index.js') return
-  jobs[key.replace(/(\.\/|\.js)/g, '')] = files(key).default
-})
-
-export default jobs
